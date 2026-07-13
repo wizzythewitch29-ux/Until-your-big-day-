a59d45a0dbedc086cfd8ca3e6f09d62325a0589f
wizzythewitch29-ux <wizzythewitch29@gmail.com>
2026-07-13 18:49:15 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,96 @@
 const birthday = new Date("August 17, 2026 00:00:00");
 
+const countdown = document.getElementById("countdown");
+const surpriseBtn = document.getElementById("surpriseBtn");
+
+const home = document.getElementById("home");
+const envelopePage = document.getElementById("envelopePage");
+const letterPage = document.getElementById("letterPage");
+
+const envelope = document.getElementById("envelope");
+
 function updateCountdown() {
+
     const now = new Date();
     const difference = birthday - now;
 
+    if (difference <= 0) {
+        countdown.innerHTML = “🎉 Happy Birthday! 🎉”;
+        return;
+    }
     const days = Math.floor(difference / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
-    document.getElementById("countdown").innerHTML =
+    const hours = Math.floor(
+        (difference % (1000 * 60 * 60 * 24)) /
+        (1000 * 60 * 60)
+    );
+
+    const minutes = Math.floor(
+        (difference % (1000 * 60 * 60)) /
+        (1000 * 60)
+    );
+
+    const seconds = Math.floor(
+        (difference % (1000 * 60)) /
+        1000
+    );
+
+    countdown.innerHTML =
         `${days} Days<br>${hours} Hours ${minutes} Minutes ${seconds} Seconds`;
+
 }
 
 updateCountdown();
 setInterval(updateCountdown, 1000);
-function openLetter() {
+/* ------- OPEN SURPRISE -——-- */
 
-    const envelope = document.getElementById("envelope");
+surpriseBtn.addEventListener(“click”, () => {
 
-    envelope.style.display = "block";
+    home.style.opacity = “0”;
 
-    envelope.onclick = function () {
+    setTimeout(() => {
 
-        envelope.classList.add("open");
+        home.classList.add(“hidden”);
 
-        setTimeout(function () {
+        envelopePage.classList.remove(“hidden”);
 
-            envelope.style.display = "none";
+        envelopePage.style.opacity = “0”;
 
-            document.getElementById("letter").style.display = "block";
+        setTimeout(() => {
 
-        }, 900);
+            envelopePage.style.opacity = “1”;
 
-    };
+        }, 100);
 
-}
+    }, 700);
 
+});
+/* ------- OPEN ENVELOPE -——-- */
+
+envelope.addEventListener(“click”, () => {
+
+    envelope.classList.add(“open”);
+
+    setTimeout(() => {
+
+        envelopePage.style.opacity = “0”;
+
+        setTimeout(() => {
+
+            envelopePage.classList.add(“hidden”);
+
+            letterPage.classList.remove(“hidden”);
+
+            letterPage.style.opacity = “0”;
+
+            setTimeout(() => {
+
+                letterPage.style.opacity = “1”;
+
+            }, 100);
+
+        }, 600);
+
+    }, 900);
+
+});
